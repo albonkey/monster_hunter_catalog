49e1a685685f762f63805a328378beff4c444119
EdwinTran <70764419+EdwinTran@users.noreply.github.com>
2022-05-06 06:19:11 -0700

    API Additions and New Monsters
    Added API implementation and also added new monster pictures

--- src/components/MyComponent/MyComponent.js
+++ src/components/MyComponent/MyComponent.js
@@ -1,61 +1,72 @@
 import React from 'react';
 import './MyComponent.scss';
 
-const MyComponent = ({ monster}) => {
-	 return(
-		 <div className='my-component'>
-		 	<div className='my-component-content'>
-				<div className='left-page'>
-					<div className='monster-name'>{monster.name}</div>
-					<div className='monster-species'>{monster.species} | <span className='monster-type'>{monster.type}</span></div>
+class MyComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			monster: props.monster
+		};
+	}
 
-					<br />
-					<img src={require(`../../assets/monster${monster.id}.png`)} className='monster-image'/>
+	render() {
+		const {monster} = this.state;
+		
+		return(
+			<div className='my-component'>
+				<div className='my-component-content'>
+					<div className='left-page'>
+						<div className='monster-name'>{monster.name}</div>
+						<div className='monster-species'>{monster.species} | <span className='monster-type'>{monster.type}</span></div>
+
+						<br />
+						<img src={require(`../../assets/monster${monster.id}.png`)} className='monster-image'/>
+					</div>
+					<div className='right-page'>
+						<div className='info-header'>Description</div>
+						<div className='info-text'>{monster.description}</div>
+						<div className='info-header'>Locations</div>
+						<div className='info-text'>
+							{
+								monster.locations.map((location, i) => {
+									if(i === 0){
+										return `${location.name}`
+									} else {
+										return `, ${location.name}`
+									}
+								})
+							}
+						</div>
+						<div className='info-header'>Protection</div>
+						<div className='info-text'>
+							{
+								monster.resistances.map(protection => {
+									return <div>
+														<div className='info-underheader'>{protection.name}</div>
+														<div>{protection.description}</div>
+												</div>
+								})
+							}
+						</div>
+						<div className='info-header'>Weaknesses</div>
+						<div className='info-text'>
+							{
+								monster.weaknesses.map((weakness, i) => {
+									if(i === 0){
+										return `${weakness.element}`
+									} else {
+										return `, ${weakness.element}`
+									}
+
+								})
+							}
+						</div>
+					</div>
+
 				</div>
-				<div className='right-page'>
-					<div className='info-header'>Description</div>
-					<div className='info-text'>{monster.description}</div>
-					<div className='info-header'>Locations</div>
-					<div className='info-text'>
-						{
-							monster.locations.map((location, i) => {
-								if(i === 0){
-									return `${location.name}`
-								} else {
-									return `, ${location.name}`
-								}
-							})
-						}
-					</div>
-					<div className='info-header'>Protection</div>
-					<div className='info-text'>
-						{
-							monster.protection.skills.map(protection => {
-								return <div>
-													<div className='info-underheader'>{protection.name}</div>
-													<div>{protection.description}</div>
-											</div>
-							})
-						}
-					</div>
-					<div className='info-header'>Weaknesses</div>
-					<div className='info-text'>
-						{
-							monster.weaknesses.map((weakness, i) => {
-								if(i === 0){
-									return `${weakness.element}`
-								} else {
-									return `, ${weakness.element}`
-								}
-
-							})
-						}
-					</div>
-				</div>
-
+				<img src={require('../../assets/book.png')} className='my-component-img'/>
 			</div>
-			<img src={require('../../assets/book.png')} className='my-component-img'/>
-		 </div>
-	 )
+		);
+	}
 }
 export default MyComponent;
